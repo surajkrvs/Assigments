be0fdd81bbf161b13666567a7fde30369b44fbdd
surajkrvs <123889474+surajkrvs@users.noreply.github.com>
2023-02-05 17:06:00 +0530

    Add files via upload

---
+++ _29-Jan-assignment  x.py
@@ -0,0 +1,33 @@
+29th jan assignment 
+
+1.Ans ; - Guido van Rossum
+2.Ans :- Python is an interpreted programming language, which supports object-oriented, structured, and functional programming.
+3.Ans:- Case is always significant while dealing with identifiers in python.
+4.Ans ;- ‘.py’ is the correct extension of the Python file. Python programs can be written in any text editor. To save these programs we need to save in files with file extension ‘.py’.
+5.Ans:- Many languages have been implemented using both compilers            and interpreters, including C, Pascal, and Python.
+6.Ans:-{}braces are used to definea block of code in python language.
+7.Ans:- # is the character used for give single line comments in python .
+8.Ans:- ‘sys’ module use to find the version of python you are currently working on .
+9.Ans:-  lambda function 
+10.Ans:- "pip" stands for "Pip Installs Packages". It is a package management system used to install and manage packages/libraries written in Python.
+11.Ans:- print(),len(),add(),type().
+12.Ans:- There is no limit on the length of an identifier in python.
+13.Ans:- Easy to learn and use . python has a smiple and intutitive syntax,making it easy for beginners to pick up and start coding.
+14.Ans ; memory management is handled by the Python memory manager, which is part of the Python interpreter. The memory manager is responsible for allocating and freeing up memory as needed, based on the type and size of objects in the program. 
+15.Ans :- few steps to use to install python in windows
+•	       1. Download the latest Python version from the official Python website.
+       2. Run the Python installation file and follow the on-screen instructions.      Make sure to select the option to add Python to the PATH environment variable during the installation process.
+      3. Once the installation is complete, open the Command Prompt and run the following command to verify that Python has been installed and the PATH variable has been set.
+  4. To set the PATH variable permanently, follow these steps:
+    * Open the Start menu and search for "Environment Variables" .
+    * Click on "Edit the system environment variables".
+    * Click on the "Environment Variables" button.
+    * Under "System Variables", scroll down and find the "Path" variable.
+    * Click on the "Edit" button.
+         * Click on the "New" button and add the path to the Python 
+         * Click "OK" to close all windows and apply the changes.
+16.Ans:- Yes , indentation is required in python.In Python , identation is used to indicate the scope of code blocks.
+
+
+
+
