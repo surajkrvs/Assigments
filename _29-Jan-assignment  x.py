0cc202e030ecd52a549bcfe2e8968600f9dd322f
surajkrvs <123889474+surajkrvs@users.noreply.github.com>
2023-02-05 17:02:34 +0530

    Add files via upload

---
+++ _29-Jan-assignment  x.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#question 1
+ans = Guido van Rossum
+
+
+# In[ ]:
+
+
+#question 2 
+ans =  interpreted programming  , Object-oriented programming  , Functional programming ,Structured programming language
+
+
+# In[ ]:
+
+
+#question 3
+ans = yes python iscase-sensitive some  || example of identifiers:- class name, function name ,variable name 
+
+
+# In[ ]:
+
+
+#question 4
+ans = .py
+
+
+# In[ ]:
+
+
+#question 5
+ans = both 
+
+
+# In[2]:
+
+
+#question 6
+ans = indentation
+  ex:-
+def num():
+    for i in range(10):
+        print(i)
+num()
+
+
+# In[ ]:
+
+
+#question 7
+ans = #(forn single line comments)
+
+
+# In[ ]:
+
+
+#question 8
+ans = pip --version (it will work on windows terminal) or sys.python
+
+
+# In[ ]:
+
+
+#question 9
+ans = lambda function
+
+
+# In[ ]:
+
+
+#question 10
+ans = package installer for Python it stand for (preferred installer program)
+
+
+# In[ ]:
+
+
+#question 11
+ans = pirnt(),sort(),int(),str(),float()
+
+
+# In[ ]:
+
+
+#question 12
+ans = 79 characters 
+
+
+# In[ ]:
+
+
+#question 13
+ans = 1.High level language 
+      2.Easy to understand as compare to another languages 
+      3.Most demanding language 
+      4.it support many domains so we can easily  switch the domains 
+
+
+# In[ ]:
+
+
+#question 14
+ans = there are two way for memory managed 
+     a.stack memory
+     b.private heap memory
+
+
+# In[ ]:
+
+
+#question 15
+ans = a. just go on python.org and download the latest version of python & after completion of download
+      b. run as administrator &
+      c.then set the path where you want put it like in C drive E,f after that
+      d.give all the permesion 
+      e.so it will easily installed 
+
+
+# In[ ]:
+
+
+#question 16
+ans = yes it is important because if we don't write the code in sequence it will through error 
+
